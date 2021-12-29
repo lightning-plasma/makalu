d903c01cfbceb8c9a756adf051b828b8bbb9007b
iiohioryuki <hiroyuki.iio@askul.com>
2021-12-29 15:10:46 +0900

    validation exceptionをhandleするようにする

--- src/main/kotlin/com/archetype/makalu/handler/ValidationErrorHandler.kt
+++ src/main/kotlin/com/archetype/makalu/handler/ValidationErrorHandler.kt
@@ -1,19 +1,17 @@
-package com.archetype.makalu.configuration
+package com.archetype.makalu.handler
 
 import io.grpc.Status
 import mu.KotlinLogging
 import org.lognet.springboot.grpc.recovery.GRpcExceptionHandler
 import org.lognet.springboot.grpc.recovery.GRpcExceptionScope
 import org.lognet.springboot.grpc.recovery.GRpcServiceAdvice
-import javax.validation.ConstraintViolationException
-
+import javax.validation.ValidationException
 
 @GRpcServiceAdvice
 class ValidationErrorHandler {
-
     // https://github.com/LogNet/grpc-spring-boot-starter#5-error-handling
     @GRpcExceptionHandler
-    fun handle(e: ConstraintViolationException, scope: GRpcExceptionScope): Status {
+    fun handle(e: ValidationException, scope: GRpcExceptionScope): Status {
         // https://github.com/LogNet/grpc-spring-boot-starter/blob/master/grpc-spring-boot-starter/src/main/java/org/lognet/springboot/grpc/recovery/ErrorHandlerAdapter.java
         logger.warn("Got error with status ${e.message}")
 
